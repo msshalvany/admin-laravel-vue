02517ae0cbcdb73a048870d782c1b0b9a3f237ae
msh <mahdisadgyanshalvany@gmail.com>
2025-01-16 07:09:50 -0600

    fix bug

--- back-end/database/migrations/2024_11_25_155523_create_loading_records_table.php
+++ back-end/database/migrations/2024_11_25_155523_create_loading_records_table.php
@@ -14,17 +14,17 @@ return new class extends Migration
         Schema::create('loading_records', function (Blueprint $table) {
             $table->id(); // شناسه بارگیری
             $table->foreignId('truck_id')->constrained('trucks'); // شناسه کامیون (رابطه با جدول کامیون‌ها)
-            $table->foreignId('company_id')->constrained('companies');
-            $table->foreignId('driver_id')->constrained('drivers');
+            $table->foreignId('company_id')->constrained('companies'); // شناسه شرکت
+            $table->foreignId('driver_id')->constrained('drivers'); // شناسه راننده
             $table->decimal('empty_weight', 8, 2); // وزن خالی کامیون
             $table->decimal('loaded_weight', 8, 2)->nullable(); // وزن بار کامیون
-            $table->string('status')->default('pending');
-            $table->timestamps('entry_date'); // زمان ورود به کارخانه
-            $table->time('entry_time'); // زمان ورود به کارخانه
-            $table->time('exit_time')->nullable()->nullable(); // زمان خروج از کارخانه
+            $table->string('status')->default('pending'); // وضعیت
+            $table->date('entry_date'); // زمان ورود به کارخانه (تاریخ)
+            $table->time('entry_time')->nullable(); // زمان ورود به کارخانه
+            $table->time('exit_time')->nullable(); // زمان خروج از کارخانه
             $table->float('driver_star')->default(0); // میزان رضایت راننده
             $table->softDeletes(); // حذف امن (soft delete)
-            $table->timestamps(); // زمان‌های ثبت و بروزرسانی
+            $table->timestamps(); // ایجاد ستون‌های created_at و updated_at
         });
     }
 
